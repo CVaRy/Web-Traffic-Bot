f501e9279dbf64e3e74a02ea672efa09b00f4833
CVaRy <cvary.61@gmail.com>
2021-11-18 23:04:34 +0300

    commit

--- index.py
+++ index.py
@@ -7,13 +7,8 @@ import multiprocessing
 from selenium.webdriver.chrome import options
 path = "./chromedriver.exe"
 proxylist = [
-"ult5.falix.gg:36856",
-"ult5.falix.gg:26272",
-#"ult8.falix.gg:36686",
-#"ult8.falix.gg:38352",
-"singapore1.qloxhost.xyz:44105",
-"frankfurt.qloxhost.xyz:33126",
-"frankfurt.qloxhost.xyz:33176",
+"ult4.falix.gg:36422",
+"ult5.falix.gg:36856"
 ]
 def visitwebsite():
     f = open("useragent.txt")
@@ -21,14 +16,18 @@ def visitwebsite():
     proxy = proxylist[random.randint(0,len(proxylist)-1)]
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('user-agent=%s' % useragent)
-    chrome_options.add_argument('--proxy-server=%s' % proxy)
+    chrome_options.add_argument('--proxy-server=socks5://%s' % proxy)
     chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.add_argument("--disable-geolocation")
+    chrome_options.add_argument("--disable-search-geolocation-disclosure")
+    chrome_options.add_argument("--scan-locations")
     chrome = webdriver.Chrome(path,chrome_options=chrome_options)
     try:
         wait_timex = 0
         hata = 0
-        wait_time = random.randrange(60,75)#Random 60-75 Second Waiting Time
-        browse = chrome.get("https://cvary.xyz/")
+        wait_time = random.randrange(160,240)#Random 60-75 Second Waiting Time
+        browse = chrome.get("https://cvary.xyz/afk-users-page/")
+
         while wait_time>wait_timex:
             wait_timex = wait_timex + 1
             time.sleep(1)
@@ -57,10 +56,16 @@ if __name__ == '__main__':
             p1 = multiprocessing.Process(target=visitwebsite)
             p2 = multiprocessing.Process(target=visitwebsite)
             p3 = multiprocessing.Process(target=visitwebsite)
+            p4 = multiprocessing.Process(target=visitwebsite)
+            p5 = multiprocessing.Process(target=visitwebsite)
+            p6 = multiprocessing.Process(target=visitwebsite)
             p1.start()
             #p2.start()
             #p3.start()
+            #p4.start()
+
 
             p1.join()
             #p2.join()
             #p3.join()
+            #p4.join()
